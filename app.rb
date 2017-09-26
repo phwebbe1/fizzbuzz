bc63222eb403b4c1dba347adb12bd94d152cce60
phwebbe1 <31141222+phwebbe1@users.noreply.github.com>
2017-09-26 14:48:33 -0400

    Add files via upload

--- app.rb
+++ app.rb
@@ -1,15 +1,26 @@
 # numbers = []
 counter = 0
-
+puts 'Enter the number to be a mod.'
+a = gets.chomp.to_i
+puts 'Enter a second number to be a mod'
+b = gets.chomp.to_i
+puts 'Enter a third number to be a mod'
+c = gets.chomp.to_i
+puts 'Enter a word to put'
+d = gets.chomp.to_i
+puts 'Enter a word to put'
+e = gets.chomp.to_i
+puts 'Enter a word to put'
+f = gets.chomp.to_i
 100.times do
 	# numbers.push(counter)
 	counter = counter +1
-	if counter % 3 == 0
-		puts "fizz"
-	elsif counter % 15 == 0
-		puts "fizzbuzz"
-	elsif counter % 5 == 0
-		puts "buzz"
+	if counter % a == 0
+		puts d
+	elsif counter % b == 0
+		puts e
+	elsif counter % c == 0
+		puts f
 	else 
 	    puts counter 
 
